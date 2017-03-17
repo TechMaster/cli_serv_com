89985fa01eff41667a926858618d1bef55e93369
TechMaster <cuong@techmaster.vn>
2017-03-17 17:33:04 +0700

    First commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const compression = require('compression')
+app.use(compression({level: 9}))
+
+
+const bodyParser = require("body-parser");
+const nunjucks = require('nunjucks');
+
+
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use('/public', express.static('public'));
+
+//Cấu hình nunjucks
+nunjucks.configure('views', {
+  autoescape: true,
+  cache: false,
+  express: app,
+  watch: true
+});
+app.engine('html', nunjucks.render);
+app.set('view engine', 'html');
+
+
+require('./router')(app);
+
+const server = app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+});
+
+app.use(function (err, req, res, next) {
+    console.error(err.stack)
+    res.status(500).send('Error: ' + err.message)
+});
